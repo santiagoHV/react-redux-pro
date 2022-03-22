f0a85a90a4947979fe7637572af4533d76864c6a
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-03-22 18:41:36 -0500

    added hooks of redux

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,35 +2,38 @@ import React, {useEffect} from 'react';
 import Searcher from '../../components/Searcher';
 import PokemonList from '../../components/PokemonList';
 import {getPokemons} from '../../api/getPokemons'
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import './styles.css';
 import { setPokemon } from '../../actions';
 
 
-//CONEXION CON EL STORE DE REDUX
-const mapStateToProps = state => ({
-  list: state.list
-})
+//CONEXION CON EL STORE DE REDUX CON CONNECT
+// const mapStateToProps = state => ({
+//   list: state.list
+// })
 
-const mapDispatchToProps = dispatch => ({
-  getPokemons: (value) => dispatch(setPokemon(value))
-})
+// const mapDispatchToProps = dispatch => ({
+//   setPokemons: (value) => dispatch(setPokemon(value))
+// })
  /////////////////
 
 
-function Home({list, setPokemons}) {
+function Home() {
+  const dispatch = useDispatch()
+  const list = useSelector(state => state.list)
+
   useEffect(()=>{
     getPokemons().then(response => {
-      setPokemons(response.results)
+      dispatch(setPokemon(response.results))
     })
   },[])
 
   return (
     <div className='Home'>
       <Searcher />
-      <PokemonList />
+      <PokemonList pokemons={list} />
     </div>
   );
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default Home;
